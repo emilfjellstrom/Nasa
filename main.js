14807854a42aebe75f1a6463ca17382fde22136a
Emil Fjellström <emilwow22@gmail.com>
2017-05-07 22:08:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ const projectNasa = ((() => ({
 
   xhr.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj;
+       var myObj = JSON.parse(this.responseText);
+       projectNasa.postError(myObj);
       }
       else if (this.status == 404){
         alert("Error!");
@@ -32,7 +32,7 @@ const projectNasa = ((() => ({
   console.log(xhr.statusText);
   },
 
-  postError() {
+  postError(myObj) {  
   if (myObj.error != null){
     document.getElementById("demo").innerHTML = `
   <input id="lon" placeholder="lon" value=""></input>
@@ -41,15 +41,15 @@ const projectNasa = ((() => ({
   alert(`The image of this section cant be shown! Try again.\n\nError Message: ${myObj.error}`);
   }
   else{
-    projectNasa.post();
+    projectNasa.post(myObj);
   };
   },
 
-  post() {
+  post(myObj) {
   document.getElementById("demo").innerHTML = `
   <input id="lon" placeholder="lon" value=""></input>
   <input id="lat" placeholder="lat" value=""></input>
-  <button onclick="valueCheck()">nasaEarth</button>
+  <button onclick="valueCheck();postNasa();">nasaEarth</button>
   <div id="democontainer" class="box">
   <img class="nasaImage" src="${myObj.url}">
   <p>Date of imagery ${myObj.date}</p>
@@ -58,43 +58,69 @@ const projectNasa = ((() => ({
 })))();
 
 function valueCheck(){
-return projectNasa.valueCheck(), projectNasa.postError();
+return projectNasa.valueCheck();
 };
-
-const addClass = document.getElementById("addBox");
-addClass.addEventListener("click", addBox);
-
-function addBox(){
-  document.getElementById("demo2container").classList.add("box");
+function postNasa(){
+return  projectNasa.postError();
 }
 
-function nasaDaily(){
-const url = "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo";
 
+const apodNasa = ((() => ({
 
-$.ajax({
-  url,
-  success(result) {
-  if("copyright" in result) {
-    $("#copyright").text(`Image Credits: ${result.copyright}`);
-  }
-  else {
-    $("#copyright").text("Image Credits: " + "Public Domain");
-  }
-  
-  if(result.media_type == "video") {
-    $("#apod_img_id").css("display", "none"); 
-    $("#apod_vid_id").attr("src", result.url);
-  }
-  else {
-    $("#apod_vid_id").css("display", "none"); 
-    $("#apod_img_id").attr("src", result.url);
-  }
-  $("#apod_explaination").text(result.explanation);
-  $("#apod_title").text(result.title);
+  Apod(){
+
+  let xhr = new XMLHttpRequest();
+  let url = `https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo`;
+
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+       apodNasa.post(myObj);
+     }
+      else if (this.status == 404){
+        alert("Error!");
+       }
+   };
+
+  xhr.open("GET", url, false);
+  xhr.send();
+  console.log(xhr.status);
+  console.log(xhr.statusText);
+
 },
-  error(error) {
-    console.log("Error", error);
- } 
-});
+  post(myObj) {
+  if (myObj.copyright != null){
+  document.getElementById("demo2").innerHTML = `
+  <div id="rightcontainer">
+  <button onclick="runApod();postApod();">nasaDaily</button>
+  <div id="demo2container" class="box">
+  <img class="nasaImage" src="${myObj.url}">
+  <p>Image Credits: ${myObj.copyright}
+  <p>Date of imagery ${myObj.date}</p>
+  <h3>${myObj.title}</h3>
+  <p>${myObj.explanation}</p>
+  </div>
+  </div>`; 
+  }
+  else{
+  document.getElementById("demo2").innerHTML = `
+  <div id="rightcontainer">
+  <button onclick="runApod();postApod();">nasaDaily</button>
+  <div id="demo2container" class="box">
+  <img class="nasaImage" src="${myObj.url}">
+  <p>Date of imagery ${myObj.date}</p>
+  <h3>${myObj.title}</h3>
+  <p>${myObj.explanation}</p>
+  </div>
+  </div>`;
+  };
+  }
+
+})))();
+
+function runApod(){
+  return apodNasa.Apod();
+};
+function postApod(){
+  return apodNasa.post();
 };
